2b28cc9012277ceca45d3f4f931bfaaf21e03d3e
pacoooh <pacoooh@gmail.com>
2020-11-19 18:18:05 +0100

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Tensor} from '@tensorflow/tfjs';
 import * as bodyPix from '@tensorflow-models/body-pix';
 import '@tensorflow/tfjs-backend-cpu';
+import { BodyPixInput } from '@tensorflow-models/body-pix/dist/types';
  @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -9,7 +10,7 @@ import '@tensorflow/tfjs-backend-cpu';
 })
 export class Tab1Page implements OnInit{
 img: HTMLImageElement;
-  
+f:BodyPixInput;
    constructor() {
 
      
@@ -24,6 +25,8 @@ async click(){
 
   //https://towardsdatascience.com/virtual-background-in-webcam-with-body-segmentation-technique-fc8106ca3038
 const net = await bodyPix.load();
+console.log(net);
+console.log(this.img);
 const segmentation = await net.segmentPerson(this.img,{
   flipHorizontal: true,
   internalResolution: 'medium',
@@ -43,6 +46,53 @@ bodyPix.drawMask(
     canvas, this.img, coloredPartImage, opacity, maskBlurAmount,
     flipHorizontal);
 }
+async click2(){
 
+  //https://towardsdatascience.com/virtual-background-in-webcam-with-body-segmentation-technique-fc8106ca3038
+ const net = await bodyPix.load();
 
+ const partSegmentation = await net.segmentMultiPersonParts(this.img);
+ 
+ console.log(partSegmentation);
+ // The mask image is an binary mask image with a 1 where there is a person and
+ // a 0 where there is not.
+ const coloredPartImage = bodyPix.toMask(partSegmentation);
+ const opacity = 0.7;
+ const flipHorizontal = false;
+ const maskBlurAmount = 0;
+ const canvas = <HTMLCanvasElement>document.getElementById('canvas');
+ // Draw the mask image on top of the original image onto a canvas.
+ // The colored part image will be drawn semi-transparent, with an opacity of
+ // 0.7, allowing for the original image to be visible under.
+ bodyPix.drawMask(
+    canvas, this.img, coloredPartImage, opacity, maskBlurAmount,
+    flipHorizontal);
+ }
+ async click3(){
+
+  //https://towardsdatascience.com/virtual-background-in-webcam-with-body-segmentation-technique-fc8106ca3038
+ const net = await bodyPix.load();
+ const segmentation = await net.segmentPerson(this.img);
+  
+ const maskBackground = true;
+ // Convert the segmentation into a mask to darken the background.
+ const foregroundColor = {r: 0, g: 0, b: 0, a: 0};
+ const backgroundColor = {r: 0, g: 0, b: 0, a: 255};
+ const backgroundDarkeningMask = bodyPix.toMask(
+     segmentation, foregroundColor, backgroundColor);
+  
+ const opacity = 0.7;
+ const maskBlurAmount = 3;
+ const flipHorizontal = false;
+ 
+ const canvas = <HTMLCanvasElement>document.getElementById('canvas');
+ // Draw the mask image on top of the original image onto a canvas.
+ // The colored part image will be drawn semi-transparent, with an opacity of
+ // 0.7, allowing for the original image to be visible under.
+ bodyPix.drawMask(
+    canvas, this.img, backgroundDarkeningMask, opacity, maskBlurAmount,
+    flipHorizontal);
+ }
+ 
+ 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,43 +1,72 @@
-import { Component } from '@angular/core';
-import * as bodyPix from '@tensorflow-models/body-pix';
-import '@tensorflow/tfjs-backend-cpu';
+import { Component, OnInit } from '@angular/core';
+ 
+
+import { CameraPreview, CameraPreviewPictureOptions, CameraPreviewOptions, CameraPreviewDimensions } from '@ionic-native/camera-preview/ngx';
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
-  img: HTMLImageElement;
-  
-  constructor() {
-
-    
+export class Tab2Page implements OnInit{
+   picture : string;
+    cameraPreviewOpts: CameraPreviewOptions = {
+    x: 0,
+    y: 0,
+    width: window.screen.width,
+    height: window.screen.height,
+    camera: 'rear',
+    tapPhoto: true,
+    previewDrag: true,
+    toBack: true,
+    alpha: 1
+  }
+  constructor(private cameraPreview: CameraPreview) {
 
   }
- ngOnInit(){
-   this.img = <HTMLImageElement>document.getElementById('image');
-console.log(this.img.width);
+ngOnInit(){
+  const cameraPreviewOpts: CameraPreviewOptions = {
+    x: 0,
+    y: 0,
+    width: window.screen.width,
+    height: window.screen.height,
+    camera: 'rear',
+    tapPhoto: true,
+    previewDrag: true,
+    toBack: true,
+    alpha: 1
+  }
+  
+  // start camera
+  this.cameraPreview.startCamera(cameraPreviewOpts).then(
+    (res) => {
+      console.log(res)
+ 
+    },
+    (err) => {
+      console.log(err)
+    });
 }
 
-async click(){
+click1(){
+  this.cameraPreview.takePicture({width:640, height:640,quality:90}).then(imageData=>{
+    // console.log(imageData);
+    this.picture = 'data:image/jpeg;base64,' + imageData;
+    console.log(this.picture);
+  });
 
- //https://towardsdatascience.com/virtual-background-in-webcam-with-body-segmentation-technique-fc8106ca3038
-const net = await bodyPix.load();
-const partSegmentation = await net.segmentMultiPersonParts(this.img);
+}
+click2(){
+  this.cameraPreview.startCamera(this.cameraPreviewOpts).then(
+    (res) => {
+      console.log(res)
+ 
+    },
+    (err) => {
+      console.log(err)
+    });
+}
+click3(){
 
-console.log(partSegmentation);
-// The mask image is an binary mask image with a 1 where there is a person and
-// a 0 where there is not.
-const coloredPartImage = bodyPix.toMask(partSegmentation);
-const opacity = 0.7;
-const flipHorizontal = false;
-const maskBlurAmount = 0;
-const canvas = <HTMLCanvasElement>document.getElementById('canvas');
-// Draw the mask image on top of the original image onto a canvas.
-// The colored part image will be drawn semi-transparent, with an opacity of
-// 0.7, allowing for the original image to be visible under.
-bodyPix.drawMask(
-   canvas, this.img, coloredPartImage, opacity, maskBlurAmount,
-   flipHorizontal);
 }
 }
